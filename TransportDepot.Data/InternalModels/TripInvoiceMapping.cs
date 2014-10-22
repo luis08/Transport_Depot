1d09a241d385980c0e639c15568d0181d768b557
luis <luis08@msn.com>
2014-10-22 18:48:17 -0400

    Missing files last commit

---
+++ TransportDepot.Data/InternalModels/TripInvoiceMapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TransportDepot.Data.InternalModels
+{
+  class TripInvoiceMapping
+  {
+    public string TripNumber { get; set; }
+    public string InvoiceNumber { get; set; }
+    public DateTime BillDate { get; set; }
+  }
+}
---
+++ TransportDepot.Models/Payables/Commissions/TripInvoiceMapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TransportDepot.Models.Payables.Commissions
+{
+  public class TripInvoiceMapping
+  {
+    public string TripNumber { get; set; }
+    public string InvoiceNumber { get; set; }
+    public DateTime BillDate { get; set; }
+  }
+}

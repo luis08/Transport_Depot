777806e07bf694bf635bf9f14ec4436b03053bf7
luis <luis08@msn.com>
2015-05-09 14:37:02 -0400

    Enhance Debugger

--- TransportDepot.Utilities/Debugger.cs
+++ TransportDepot.Utilities/Debugger.cs
@@ -1,10 +1,37 @@
-﻿
-using System.IO;
+﻿using System.IO;
+using System;
 namespace TransportDepot.Utilities
 {
   public static class Debugger
   {
-    private const string path = @"C:\Transport_Depot\debug_wcf.txt";
+    private const string path = @"C:\Sites\debug_wcf.txt";
+    private const int DebugDelimiterTextWidth = 100;
+    public static void WriteDebugHeader(string headerText)
+    {
+      Write(new string('*', DebugDelimiterTextWidth));
+      var fullHeaderText = string.Format("** {0: yyyy-MM-dd hh:mm:ss.fff} Debug Section Start: {1} *******", DateTime.Now, headerText);
+      fullHeaderText = string.Concat(fullHeaderText,
+        new string('*', DebugDelimiterTextWidth - fullHeaderText.Length));
+      Write(fullHeaderText);
+      Write(new string('*', DebugDelimiterTextWidth));
+      Write(Environment.NewLine);
+      Write(Environment.NewLine);
+    }
+
+    public static void WriteDebugFooter(string footerText)
+    {
+      Write(new string('*', DebugDelimiterTextWidth));
+      var fullHeaderText = string.Format("** {0: yyyy-MM-dd hh:mm:ss.fff} Debug Section END: {1} *******", DateTime.Now, footerText);
+      fullHeaderText = string.Concat(fullHeaderText,
+        new string('*', DebugDelimiterTextWidth - fullHeaderText.Length));
+      Write(fullHeaderText);
+      Write(new string('*', DebugDelimiterTextWidth));
+      Write(new string('*', DebugDelimiterTextWidth));
+      Write(new string('*', DebugDelimiterTextWidth));
+      Write(Environment.NewLine);
+      Write(Environment.NewLine);
+    }
+
     public static void Write(string text)
     {
       using (StreamWriter sw = File.AppendText(path))

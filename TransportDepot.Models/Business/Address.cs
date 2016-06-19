501d5cbe1f292583c3d180175b063525291d360e
luis <luis08@msn.com>
2016-06-19 16:05:25 -0400

    Small change

--- TransportDepot.Models/Business/Address.cs
+++ TransportDepot.Models/Business/Address.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TransportDepot.Models.Business
+﻿namespace TransportDepot.Models.Business
 {
   public class Address
   {
-    public String StreetAddress { get; set; }
+    public string StreetAddress { get; set; }
 
     public string City { get; set; }
 
